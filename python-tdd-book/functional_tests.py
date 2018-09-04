b360970d031ecc30ed2cc1afd26bb089e595ce39
Edson Yamamoto <edsonkazyamamoto@gmail.com>
2018-09-04 14:24:24 -0300

    atualizado os testes para classes

--- python-tdd-book/functional_tests.py
+++ python-tdd-book/functional_tests.py
@@ -1,6 +1,44 @@
+
+    #novo aplicativo online chamado To-Do, verifica a nova pagina
+
+    #Existe o nome 'To-Do' na pagina
+
+    #receber o invite para entrar no APP de modo unico
+
+    #Ela escreve "Buy peacock feathers" numa text box 'robby do usuario'
+
+    #Quando apertar enter, da um update da pagina, e agora lista da pagina posssui o item "1:buy peacock feathers" como item  da lista de to-do
+
+    #Então deve existir uma caixa de texto para convidar a adicionar outro item. ela escreve "use peacock feather to make a fly" o usuario é muito metódico
+
+    #a pagina deve dar update novamente, e agora os 2 itens devem ser mostrados
+
+    # o usuario se perguntar se o site vai conter as informações ao relogar na pagina
+
+    #o site deverá ter uma URL unica
+
+    # ela visita aquela URL e verifica que seus itens ainda estão salvos
+
+    #satisfeita ela volta a dormir
+
 from selenium import webdriver
+import unittest
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+    def tearDown(self):
+        self.browser.quit()
 
-assert 'Django' in browser.title
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #check home page
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('finish the test!')
+
+        # She is invited to enter a to-do item straight away
+        ##[...rest of comments as before]
+
+if __name__ == '__main__':  
+    unittest.main(warnings='ignore') 
