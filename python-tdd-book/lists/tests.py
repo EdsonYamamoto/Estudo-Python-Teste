461a1035a04cf7ca422efe289fa68a4de88631d5
Edson Yamamoto <edsonkazyamamoto@gmail.com>
2018-09-04 16:33:47 -0300

    testes para home

--- python-tdd-book/lists/tests.py
+++ python-tdd-book/lists/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
+
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -10,6 +12,7 @@ class HomePageTest(TestCase):
     
 
     def test_home_page_returns_correct_html(self):
+        print("teste1")
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf8')
@@ -17,3 +20,24 @@ class HomePageTest(TestCase):
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
+
+    def test_home_page_returns_correct_html(self):
+        print("teste 2")
+        response = self.client.get('/')  
+
+        html = response.content.decode('utf8')  
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')
