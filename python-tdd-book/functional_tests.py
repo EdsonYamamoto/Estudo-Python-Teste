d7018893e2b12e87bc655cfc1f6a9f0b6367f4a6
Edson Yamamoto <edsonkazyamamoto@gmail.com>
2018-09-05 10:37:21 -0300

    terminado aula 4

--- python-tdd-book/functional_tests.py
+++ python-tdd-book/functional_tests.py
@@ -22,6 +22,8 @@
     #satisfeita ela volta a dormir
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -45,21 +47,20 @@ class NewVisitorTest(unittest.TestCase):
             'Enter a to-do item'
         )
 
-        #http://www.obeythetestinggoat.com/book/chapter_philosophy_and_refactoring.html
         #<input id="id_new_item" placeholder="Enter a to-do item" />
         #<table id="id_list_table">
         #</table>
         #</body>
  
-        inputbox = send_keys('Buy peacock feathers')
-        # She is invited to enter a to-do item straight away
-        ##[...rest of comments as before]
+        inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.slee(1)
+        time.sleep(1)
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
+        rows = table.find_elements_by_tag_name('tr')
+        
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
         #self.fail('Finish test!')
 
