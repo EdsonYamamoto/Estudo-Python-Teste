ada908ac9109b32cd73ab79592676571e52b4102
Edson Yamamoto <edsonkazyamamoto@gmail.com>
2018-09-06 09:07:36 -0300

    alterações assertion inserir informações do todo usuario

--- python-tdd-book/functional_tests.py
+++ python-tdd-book/functional_tests.py
@@ -25,11 +25,13 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
+import time
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
+        time.sleep(4)
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -54,6 +56,8 @@ class NewVisitorTest(unittest.TestCase):
  
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         #self.assertTrue(
@@ -62,6 +66,17 @@ class NewVisitorTest(unittest.TestCase):
         #)
         self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         #self.fail('Finish test!')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly',[row.text for row in rows])
+
+        self.fail("Finish the test!")
 
 if __name__ == '__main__':  
     unittest.main(warnings='ignore') 
